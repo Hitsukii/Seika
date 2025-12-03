432c142819cf27044203198d05a4f38ca1fe6523
Hitsukii <69397623+Hitsukii@users.noreply.github.com>
2025-12-02 23:37:38 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,14 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, se
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "seika-ba8ac.firebaseapp.com",
-  projectId: "seika-ba8ac",
-  storageBucket: "seika-ba8ac.firebasestorage.app",
-  messagingSenderId: "554814845878",
-  appId: "1:554814845878:web:75f4ba517042cf0b680717"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
