6ad1f23f22946124077b6041c84dff12ed5ffaa1
Hitsukii <69397623+Hitsukii@users.noreply.github.com>
2025-12-02 21:50:39 -0300

    Update app.js
    update papipo

--- app.js
+++ app.js
@@ -1,34 +1,67 @@
-import {auth,db,logout,onUserStateChanged} from './auth.js';
-import {collection,addDoc,query,where,getDocs,doc,updateDoc} 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { auth, db, logout, onUserStateChanged } from './auth.js';
 
-onUserStateChanged(async(user)=>{
- if(!user) location='login.html';
- else{
-   loadGoals(user.uid);
-   document.getElementById('add').onclick=()=>createGoal(user.uid);
- }
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Detecta si hay usuario logueado
+onUserStateChanged(async (user) => {
+  if (!user) {
+    location = 'login.html';
+  } else {
+    loadGoals(user.uid);
+    document.getElementById('add').onclick = () => createGoal(user.uid);
+  }
 });
 
-async function loadGoals(uid){
- const qs=query(collection(db,'goals'),where('uid','==',uid));
- const snap=await getDocs(qs);
- const div=document.getElementById('goals'); div.innerHTML='';
- snap.forEach(d=>{
-  const g=d.data();
-  div.innerHTML+=`<div><b>${g.name}</b><br>Racha: ${g.streak} días<br>
-  <button onclick="mark('${d.id}',${g.streak})">Marcar día</button></div><hr>`;
- });
+// Cargar metas del usuario
+async function loadGoals(uid) {
+  const qs = query(collection(db, 'goals'), where('uid', '==', uid));
+  const snap = await getDocs(qs);
+
+  const div = document.getElementById('goals');
+  div.innerHTML = '';
+
+  snap.forEach(d => {
+    const g = d.data();
+
+    div.innerHTML += `
+      <div>
+        <b>${g.name}</b><br>
+        Racha: ${g.streak} días<br>
+        <button onclick="mark('${d.id}', ${g.streak})">Marcar día</button>
+      </div>
+      <hr>
+    `;
+  });
 }
 
-window.mark=async(id,streak)=>{
- await updateDoc(doc(db,'goals',id),{streak:streak+1,last:Date.now()});
- location.reload();
-}
+// Marcar racha
+window.mark = async (id, streak) => {
+  await updateDoc(doc(db, 'goals', id), {
+    streak: streak + 1,
+    last: Date.now()
+  });
+  location.reload();
+};
 
-async function createGoal(uid){
- const name=prompt("Nombre de meta:");
- if(!name) return;
- await addDoc(collection(db,'goals'),{uid,name,streak:0,last:null});
- location.reload();
+// Crear meta nueva
+async function createGoal(uid) {
+  const name = prompt("Nombre de meta:");
+  if (!name) return;
+
+  await addDoc(collection(db, 'goals'), {
+    uid,
+    name,
+    streak: 0,
+    last: null
+  });
+
+  location.reload();
 }
